f279b662205fe9031fd80dffe0f8d9bf4664d5b4
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2017-02-06 21:15:50 -0500

    added trip and photo views

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,14 @@
 class PhotosController < ApplicationController
+	def create
+		@trip = Trip.find(params[:trip_id])
+		@photo = @trip.photos.create(photo_params)
+		
+		redirect_to trip_path(@trip)
+	end
+	
+	private
+	def photo_params
+		params.require(:photo).permit(:description, :photo_url)
+	end
+	
 end
