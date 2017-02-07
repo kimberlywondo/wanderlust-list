4632bea3e674e9704effdf3ebbb36a6c99732907
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2017-02-06 20:14:58 -0500

    added CanCan authorization

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,34 @@
 class TripsController < ApplicationController
+	load_and_authorize_resource only: [:edit, :update, :destroy]
+	
 	def index
 		@trips = Trip.all
+	end
+	
+	def show
+		@user = current_user
+		@trip = Trip.find(params[:id])
+	end
+	
+	def new
+		@user = current_user
+    @trip = Trip.new
+	end 
+	
+	def edit
+	end
+	
+	def create
+	end
+	
+	def update
+	end
+	
+	def destroy
+	end
+	
+	private 
+	def trip_params
+		params.require(:trip).permit(:city, :country)
 	end
 end
