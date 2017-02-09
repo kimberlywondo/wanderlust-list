1ab8e3c2fc739ab86b088db56014ebfe0ac76eb7
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2017-02-08 21:42:33 -0500

    added username and username validation to devise model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+	
+	validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 end
