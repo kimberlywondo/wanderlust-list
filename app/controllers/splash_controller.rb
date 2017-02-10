37530f3fcc8cd425c4109253a2eaac868692be66
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2017-02-09 20:58:16 -0500

    added signin and login modals to splash pagegit add .

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,4 +1,5 @@
 class SplashController < ApplicationController
+	before_filter :authenticate_user!, except: [:index]
 	def index
 		@trips = Trip.all
 #		@trips = Trip.pluck(:city).uniq
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'registrations'}
+  root 'splash#index'
+	devise_for :users, :controllers => {registrations: 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root 'splash#index'
+	
 	get '/trips/explore', to: 'trips#explore'
 	resources :trips do
 		resources :photos
