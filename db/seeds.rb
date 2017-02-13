00c4cf2935f831d331dbba4372b4b22264b62be3
Kimberly Wondoloski <kimberly.wondoloski@gmail.com>
2017-02-13 11:00:14 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
 User.create!([
-  {email: "kw@test.com", encrypted_password: "$2a$11$764uyKSuC9GRrXCfAFhYbu4U4nrFuSkS.18sKzsutS3KK67ksBkqu", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2017-02-13 04:37:22", last_sign_in_at: "2017-02-13 04:37:22", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", username: "kimberlywondo"}
+  {email: "k@test.com", encrypted_password: "$2a$11$Y9D4LJot4aNofZZmxRN7O.6GC7G6Ywl5NzwlJnwc1kLC2it0pD.cG", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2017-02-13 15:58:25", last_sign_in_at: "2017-02-13 15:58:25", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", username: "kimberlywondo"}
 ])
 Photo.create!([
-  {description: "blahhhh", trip_id: 1, photo_url: nil, image: nil},
-  {description: "photo 1", trip_id: 1, photo_url: nil, image: "jkjkljkl.jpg"}
+  {description: "Lovely!", trip_id: 1, photo_url: nil, image: "S_122002.jpg"}
 ])
 Trip.create!([
-  {city: "Cape town", country: "south africa", user_id: 1}
+  {city: "Cape Town ", country: "South Africa", user_id: 1}
 ])
